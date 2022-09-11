f798d6622f43d7a4ef7b8fefd0ddc68008081160
Стив <kenethra.74@gmail.com>
2022-09-11 15:14:36 -0600

    feat(feature-user): cors and pipe validation

--- apps/feature-user/src/main.ts
+++ apps/feature-user/src/main.ts
@@ -3,16 +3,19 @@
  * This is only a minimal backend to get started.
  */
 
-import { Logger } from '@nestjs/common';
+import { Logger, ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 
 import { AppModule } from './app/app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.useGlobalPipes(new ValidationPipe());
+  app.enableCors();
   const globalPrefix = 'api';
   app.setGlobalPrefix(globalPrefix);
   const port = process.env.PORT || 3334;
+
   await app.listen(port);
   Logger.log(
     `🚀 Application is running on: http://localhost:${port}/${globalPrefix}`
