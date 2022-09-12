e87693b10d4a1328ede6bb6d0107412a45f59bcd
Стив <kenethra.74@gmail.com>
2022-09-12 14:55:30 -0600

    feat(feat-profile): unfollow route

--- apps/feature-profile/src/app/profile.controller.ts
+++ apps/feature-profile/src/app/profile.controller.ts
@@ -1,13 +1,24 @@
 import { JwtAuthGuard } from '@microservice-app/auth';
 import { ProfileDto } from '@microservice-app/models';
-import { Controller, Get, Param, Post, Req, UseGuards } from '@nestjs/common';
+import {
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Post,
+  Req,
+  UseGuards,
+} from '@nestjs/common';
 
 import { ProfileService } from './profile.service';
 
-@Controller()
+@Controller('/profiles')
 export class ProfileController {
   constructor(private readonly profileService: ProfileService) {}
 
+  /**
+   * Returns user profileService
+   */
   @UseGuards(JwtAuthGuard)
   @Get('/:username')
   getProfile(
@@ -17,6 +28,12 @@ export class ProfileController {
     return this.profileService.findOne(username, req.user);
   }
 
+  /**
+   * Adds a follow to a username
+   *
+   * @param username the username to follow
+   * @returns the updated profile ProfileDto
+   */
   @UseGuards(JwtAuthGuard)
   @Post('/:username/follow')
   addFavorite(
@@ -24,5 +41,20 @@ export class ProfileController {
     @Param('username') username: string
   ): Promise<ProfileDto | null> {
     return this.profileService.modifyFollow(username, req.user, 'FOLLOW');
+  }
+
+  /**
+   * Remove a follow to a username
+   *
+   * @param username the username to follow
+   * @returns the updated profile ProfileDto
+   */
+  @UseGuards(JwtAuthGuard)
+  @Delete('/:username/unfollow')
+  removeFavorite(
+    @Req() req: any,
+    @Param('username') username: string
+  ): Promise<ProfileDto | null> {
+    return this.profileService.modifyFollow(username, req.user, 'UNFOLLOW');
   }
 }
