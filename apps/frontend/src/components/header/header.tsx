7d9619b1c233ab133520e0370bc13ea5e552b415
Стив <kenethra.74@gmail.com>
2022-09-20 22:00:53 -0600

    feat(frontend): logout handler

--- apps/frontend/src/components/header/header.tsx
+++ apps/frontend/src/components/header/header.tsx
@@ -1,5 +1,9 @@
-import { useEffect } from 'react';
+import { Button } from '@mui/material';
+import { useContext, useEffect } from 'react';
 import { NavLink, useNavigate } from 'react-router-dom';
+import { ConfigContext } from '../../context/routesContext';
+import { logoutAction } from '../../store/authStore';
+import { useAppDispatch, useAppSelector } from '../../store/hooks';
 import {
   StyledAppBar,
   StyledMenuItem,
@@ -11,6 +15,9 @@ export interface HeaderProps {}
 
 export function Header(props: HeaderProps) {
   const navigate = useNavigate();
+  const dispatch = useAppDispatch();
+  const { userData, isUserLoggedIn } = useAppSelector((state) => state.auth);
+  const { clearToken } = useContext(ConfigContext);
 
   const userToken = localStorage.getItem('access_token');
 
@@ -19,6 +26,13 @@ export function Header(props: HeaderProps) {
       navigate('/login');
     }
   }, []);
+
+  const logoutHandler = () => {
+    localStorage.removeItem('access_token');
+    dispatch(logoutAction());
+    clearToken();
+    navigate('/login');
+  };
 
   return (
     <StyledAppBar position="static">
@@ -42,6 +56,19 @@ export function Header(props: HeaderProps) {
               <StyledMenuItem component={NavLink} to="/register">
                 Register
               </StyledMenuItem>
+            </>
+          )}
+        </ToolbarInnerContainer>
+        <ToolbarInnerContainer>
+          {userToken && (
+            <>
+              <Button
+                variant="contained"
+                color="secondary"
+                onClick={logoutHandler}
+              >
+                Logout
+              </Button>
             </>
           )}
         </ToolbarInnerContainer>
