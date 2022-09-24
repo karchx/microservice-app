4115c8d63584cb13e8b7e90c859d3c2da83ddbb9
Стив <kenethra.74@gmail.com>
2022-09-24 17:45:44 -0600

    feat(libs:) codegen user by email query

--- libs/data-access/src/lib/autogenerate/hooks.tsx
+++ libs/data-access/src/lib/autogenerate/hooks.tsx
@@ -3,8 +3,18 @@ import * as Types from './operations';
 import { gql } from '@apollo/client';
 import * as Apollo from '@apollo/client';
 const defaultOptions = {} as const;
-// Generated on 2022-09-24T17:39:23-06:00
+// Generated on 2022-09-24T17:45:28-06:00
 
+export const UserFragmentDoc = gql`
+    fragment user on User {
+  _id
+  bio
+  email
+  image
+  username
+  updatedAt
+}
+    `;
 export const AuthorFragmentDoc = gql`
     fragment author on User {
   _id
@@ -35,16 +45,19 @@ export const ArticleWithCommentsFragmentDoc = gql`
   }
 }
     ${CommentWithAuthorFragmentDoc}`;
-export const UserFragmentDoc = gql`
-    fragment user on User {
+export const UserWithArticlesFragmentDoc = gql`
+    fragment userWithArticles on User {
   _id
   bio
   email
   image
   username
   updatedAt
+  articles {
+    ...articleWithComments
+  }
 }
-    `;
+    ${ArticleWithCommentsFragmentDoc}`;
 export const ArticlesDocument = gql`
     query articles($input: String) {
   articles(slug: $input) {
@@ -126,4 +139,42 @@ export type MeLazyQueryHookResult = ReturnType<typeof useMeLazyQuery>;
 export type MeQueryResult = Apollo.QueryResult<Types.MeQuery, Types.MeQueryVariables>;
 export function refetchMeQuery(variables?: Types.MeQueryVariables) {
       return { query: MeDocument, variables: variables }
+    }
+export const UserByEmaildDocument = gql`
+    query userByEmaild($input: String) {
+  user(email: $input) {
+    ...userWithArticles
+  }
+}
+    ${UserWithArticlesFragmentDoc}`;
+
+/**
+ * __useUserByEmaildQuery__
+ *
+ * To run a query within a React component, call `useUserByEmaildQuery` and pass it any options that fit your needs.
+ * When your component renders, `useUserByEmaildQuery` returns an object from Apollo Client that contains loading, error, and data properties
+ * you can use to render your UI.
+ *
+ * @param baseOptions options that will be passed into the query, supported options are listed on: https://www.apollographql.com/docs/react/api/react-hooks/#options;
+ *
+ * @example
+ * const { data, loading, error } = useUserByEmaildQuery({
+ *   variables: {
+ *      input: // value for 'input'
+ *   },
+ * });
+ */
+export function useUserByEmaildQuery(baseOptions?: Apollo.QueryHookOptions<Types.UserByEmaildQuery, Types.UserByEmaildQueryVariables>) {
+        const options = {...defaultOptions, ...baseOptions}
+        return Apollo.useQuery<Types.UserByEmaildQuery, Types.UserByEmaildQueryVariables>(UserByEmaildDocument, options);
+      }
+export function useUserByEmaildLazyQuery(baseOptions?: Apollo.LazyQueryHookOptions<Types.UserByEmaildQuery, Types.UserByEmaildQueryVariables>) {
+          const options = {...defaultOptions, ...baseOptions}
+          return Apollo.useLazyQuery<Types.UserByEmaildQuery, Types.UserByEmaildQueryVariables>(UserByEmaildDocument, options);
+        }
+export type UserByEmaildQueryHookResult = ReturnType<typeof useUserByEmaildQuery>;
+export type UserByEmaildLazyQueryHookResult = ReturnType<typeof useUserByEmaildLazyQuery>;
+export type UserByEmaildQueryResult = Apollo.QueryResult<Types.UserByEmaildQuery, Types.UserByEmaildQueryVariables>;
+export function refetchUserByEmaildQuery(variables?: Types.UserByEmaildQueryVariables) {
+      return { query: UserByEmaildDocument, variables: variables }
     }
--- libs/data-access/src/lib/autogenerate/operations.tsx
+++ libs/data-access/src/lib/autogenerate/operations.tsx
@@ -1,6 +1,6 @@
 import * as Types from './types';
 
-// Generated on 2022-09-24T17:39:23-06:00
+// Generated on 2022-09-24T17:45:28-06:00
 
 export type ArticleWithCommentsFragment = { __typename?: 'Article', _id: string, body: string, description: string, favoritesCount: number, tagList: Array<string | null>, title: string, comments: Array<{ __typename?: 'Comment', _id: string, body: string, createdAt: string, author: { __typename?: 'User', _id: string, image?: string | null, username: string } }> };
 
@@ -9,6 +9,8 @@ export type CommentWithAuthorFragment = { __typename?: 'Comment', _id: string, b
 export type UserFragment = { __typename?: 'User', _id: string, bio?: string | null, email: string, image?: string | null, username: string, updatedAt: string };
 
 export type AuthorFragment = { __typename?: 'User', _id: string, image?: string | null, username: string };
+
+export type UserWithArticlesFragment = { __typename?: 'User', _id: string, bio?: string | null, email: string, image?: string | null, username: string, updatedAt: string, articles: Array<{ __typename?: 'Article', _id: string, body: string, description: string, favoritesCount: number, tagList: Array<string | null>, title: string, comments: Array<{ __typename?: 'Comment', _id: string, body: string, createdAt: string, author: { __typename?: 'User', _id: string, image?: string | null, username: string } }> }> };
 
 export type ArticlesQueryVariables = Types.Exact<{
   input?: Types.InputMaybe<Types.Scalars['String']>;
@@ -21,3 +23,10 @@ export type MeQueryVariables = Types.Exact<{ [key: string]: never; }>;
 
 
 export type MeQuery = { __typename?: 'Query', me: { __typename?: 'User', _id: string, bio?: string | null, email: string, image?: string | null, username: string, updatedAt: string, feed: Array<{ __typename?: 'Article', _id: string, body: string, description: string, favoritesCount: number, tagList: Array<string | null>, title: string, comments: Array<{ __typename?: 'Comment', _id: string, body: string, createdAt: string, author: { __typename?: 'User', _id: string, image?: string | null, username: string } }> }>, articles: Array<{ __typename?: 'Article', _id: string, body: string, description: string, favoritesCount: number, tagList: Array<string | null>, title: string, comments: Array<{ __typename?: 'Comment', _id: string, body: string, createdAt: string, author: { __typename?: 'User', _id: string, image?: string | null, username: string } }> }> } };
+
+export type UserByEmaildQueryVariables = Types.Exact<{
+  input?: Types.InputMaybe<Types.Scalars['String']>;
+}>;
+
+
+export type UserByEmaildQuery = { __typename?: 'Query', user: { __typename?: 'User', _id: string, bio?: string | null, email: string, image?: string | null, username: string, updatedAt: string, articles: Array<{ __typename?: 'Article', _id: string, body: string, description: string, favoritesCount: number, tagList: Array<string | null>, title: string, comments: Array<{ __typename?: 'Comment', _id: string, body: string, createdAt: string, author: { __typename?: 'User', _id: string, image?: string | null, username: string } }> }> } };
