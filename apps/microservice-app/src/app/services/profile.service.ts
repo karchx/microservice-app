e1e7f66f7d89a8ca500b34d1fb533d661678b84b
Стив <kenethra.74@gmail.com>
2022-09-24 18:06:17 -0600

    feat(microservice-app): profile service

--- apps/microservice-app/src/app/services/profile.service.ts
+++ apps/microservice-app/src/app/services/profile.service.ts
@@ -1,4 +1,33 @@
+import { ProfileDto } from '@microservice-app/models';
+import { PromisifyHttpService } from '@microservice-app/shared';
 import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 
 @Injectable()
-export class ProfileService {}
+export class ProfileService {
+  profileFeatureBaseUrl: string;
+
+  constructor(
+    private configSerive: ConfigService,
+    private promisifyHttp: PromisifyHttpService
+  ) {
+    this.profileFeatureBaseUrl = this.configSerive.get<string>(
+      'features.profile.baseUrl'
+    );
+  }
+
+  async getProfile(username: string, authHeader: any): Promise<ProfileDto> {
+    const url = `${this.profileFeatureBaseUrl}/profiles/${username}`;
+    return await this.promisifyHttp.get(url, { headers: authHeader });
+  }
+
+  async addFollow(username: string, authHeader: any): Promise<ProfileDto> {
+    const url = `${this.profileFeatureBaseUrl}/profiles/${username}/follow`;
+    return await this.promisifyHttp.post(url, {}, { headers: authHeader });
+  }
+
+  async removeFollow(username: string, authHeader: any): Promise<ProfileDto> {
+    const url = `${this.profileFeatureBaseUrl}/profiles/${username}/unfollow`;
+    return await this.promisifyHttp.delete(url, { headers: authHeader });
+  }
+}
